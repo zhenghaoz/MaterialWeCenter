5f5c99fe09b101fe61cba94329ed72221e4c0624
ZhangZhenghao <zhangzhenghao@hotmail.com>
2015-11-25 20:00:32 +0800

    Add login activity

--- app/src/main/java/com/zjut/material_wecenter/controller/activity/MainActivity.java
+++ app/src/main/java/com/zjut/material_wecenter/controller/activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.zjut.material_wecenter.controller.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 
+import com.squareup.picasso.Picasso;
+import com.zjut.material_wecenter.Client;
+import com.zjut.material_wecenter.Config;
 import com.zjut.material_wecenter.R;
 import com.zjut.material_wecenter.controller.fragment.ExploreFragment;
+import com.zjut.material_wecenter.models.LoginProcess;
 
 import br.liveo.Model.HelpLiveo;
 import br.liveo.interfaces.OnItemClickListener;
@@ -15,13 +22,29 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
 
     private HelpLiveo mHelpLiveo;
 
+    private String uid;
+    private String user_name;
+    private String password;
+    private String avatar_file;
+
     // Fragments
     private ExploreFragment exploreFragment;
 
     @Override
     public void onInt(Bundle bundle) {
 
+        // Read shared preference
+        SharedPreferences preferences = getSharedPreferences("account", MODE_PRIVATE);
+        uid = preferences.getString("uid", "");
+        user_name = preferences.getString("user_name", "");
+        password = preferences.getString("password", "");
+        avatar_file = preferences.getString("avatar_file", "");
+        new UserLoginTask().execute();
+
         // Creating items navigation
+        this.userName.setText(user_name);
+        this.userEmail.setText(uid);
+        Picasso.with(this).load(Config.AVATAR_DIR + avatar_file).into(this.userPhoto);
         mHelpLiveo = new HelpLiveo();
         mHelpLiveo.add(getString(R.string.dynamic), R.mipmap.ic_notifications_on_grey600_48dp);
         mHelpLiveo.add(getString(R.string.explore), R.mipmap.ic_explore_grey600_48dp);
@@ -33,12 +56,15 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
                 .setOnClickUser(onClickPhoto)
                 .setOnClickFooter(onClickFooter)
                 .build();
+
     }
 
     private View.OnClickListener onClickPhoto = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             closeDrawer();
+            Intent intent = new Intent(MainActivity.this, UserActivity.class);
+            startActivity(intent);
         }
     };
 
@@ -66,5 +92,19 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
                 setTitle(R.string.explore);
         }
         transaction.commit();
+    }
+
+    private class UserLoginTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            LoginProcess response = Client.LoginProcess(uid, password);
+            if (response == null || response.getErrno() != 1) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            return null;
+        }
     }
 }

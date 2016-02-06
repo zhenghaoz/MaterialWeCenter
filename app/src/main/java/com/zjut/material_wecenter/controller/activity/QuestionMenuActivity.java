e1ad9ee301470a59f1648f993fb04daa6b376b7a
陈鸿发 <storewind@126.com>
2016-02-06 14:04:23 +0800

    增加对答案的点赞和感谢功能。

--- app/src/main/java/com/zjut/material_wecenter/controller/activity/QuestionMenuActivity.java
+++ app/src/main/java/com/zjut/material_wecenter/controller/activity/QuestionMenuActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nispok.snackbar.Snackbar;
 import com.zjut.material_wecenter.Client;
 import com.zjut.material_wecenter.Config;
 import com.zjut.material_wecenter.R;
@@ -18,27 +20,29 @@ import java.util.ArrayList;
 
 public class QuestionMenuActivity extends Activity implements View.OnClickListener {
 
-    private int resultCode=0;
+    private int mResultCode=0;
     private int questionID;
     private String questionTitle;
-    private View focus;
+    private View focus;//关注按钮
     private TextView focusText;
-    private View thanks;
+    private View thanks;//感谢按钮
     private TextView thanksText;
-    private View answer;
-    private View invite;
-    private View close;
+    private View answer;//回答按钮
+    private View invite;//邀请按钮
+    private View close;//关闭按钮
 
-    private int isFocus;
-    private int isThanks;
-    private int isAllowToAnswer;
+    private int isFocus;//是否已关注
+    private int isThanks;//是否已感谢
+    private int isAllowToAnswer;//能否继续回答该问题
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_menu);
         getWindow().setLayout(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        getWindow().setGravity(Gravity.BOTTOM);
 
+        //get intent
         Intent intent=getIntent();
         questionID=intent.getIntExtra("questionID", -1);
         questionTitle=intent.getStringExtra("questionTitle");
@@ -46,6 +50,7 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
         isThanks=intent.getIntExtra("isThanks",-1);
         isAllowToAnswer=intent.getIntExtra("isAllowToAnswer", -1);
 
+        //init buttons
         focus=findViewById(R.id.fab_question_focus);
         focus.setOnClickListener(this);
         focusText=(TextView) findViewById(R.id.textView_focus);
@@ -67,6 +72,17 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //回答后设置返回值来更新
+        if(resultCode==1){
+            setResult(resultCode);
+            finish();
+        }
+
+    }
+
+    @Override
     public void onClick(View v) {
         Intent intent;
         switch (v.getId()){
@@ -81,7 +97,7 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
                     intent=new Intent(this,PostAnswerActivity.class);
                     intent.putExtra("questionID",questionID);
                     intent.putExtra("questionTitle",questionTitle);
-                    startActivityForResult(intent,resultCode);
+                    startActivityForResult(intent,mResultCode);
                 }
                 else {
                     Toast.makeText(this,"对不起，您不可以再继续回答该问题了！",Toast.LENGTH_LONG).show();
@@ -96,6 +112,7 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
         }
     }
 
+    //异步进行操作
     private class DoAction extends AsyncTask<Integer,Integer,Integer> {
 
         private static final int FOCUS=0;
@@ -147,7 +164,6 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
                     else if(mFocus.getType().equals("remove")){
                         focusText.setText("关注");
                     }
-                    else Toast.makeText(QuestionMenuActivity.this,"操作失败",Toast.LENGTH_LONG);
                     break;
                 case THANKS:
                     thanksText.setText("已感谢");
@@ -155,6 +171,10 @@ public class QuestionMenuActivity extends Activity implements View.OnClickListen
                 default:
                     break;
             }
+            if (result == null) // 未知错误
+                Snackbar.with(QuestionMenuActivity.this).text("未知错误").show(QuestionMenuActivity.this);
+            else                // 显示错误
+                Snackbar.with(QuestionMenuActivity.this).text(result.getErr()).show(QuestionMenuActivity.this);
         }
     }
 
